f99c4b0071c457da9416a3307850c96185607568
emre <emre.er@avl.com>
2019-03-21 20:42:53 +0300

    initial / route setting

--- app.js
+++ app.js
@@ -3,4 +3,13 @@ const logger = require('./utils/logger');
 const logger_test = require('./tests/logger_test');
 const config_test = require('./tests/config_test');
 
+var express= require('express');
+var app = express();
 
+var routes = require('./routes/routes')
+app.use('/', routes)
+
+var server = app.listen(3000, function(){
+    console.log('Listening on port ' + server.address().port);
+  });
+  
