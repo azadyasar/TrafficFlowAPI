e26c3045397173f9ee62f3394320eb1d51b6c314
azadyasar <ceazadyasar@gmail.com>
2019-03-24 16:29:03 +0300

    Integrated babel. Babel allows us to transpile our ES6 JS code to ES5 for nodejs

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const config = require('config');
+require('dotenv').config();
+
+console.log(`Name: ${config.get('name')}`);
+console.log(`Secret key from dotenv: ${process.env.JWT_SECRET_KEY}`);
+console.log(config.get('jwt_secret_key'));
+
+console.log(process.env.NODE_ENV);
+
+var express = require('express');
+var app = express();
+
+var server = app.listen(3000, function(){
+    console.log('Listening on port ' + server.address().port);
+  });
+  
